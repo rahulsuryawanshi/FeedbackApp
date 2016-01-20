6ae25b10ff7af97e7cc5cc79c98c2753d7213f22
Rahul <rahul.suryawanshi@outlook.com>
2016-01-20 11:32:08 -0500

    Working on changes for Chrome Control.

---
+++ FeedbackApp/App/Layout/spAppChrome.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="d:\rahul\poc\sharepointhosted\feedbackapp\feedbackapp\scripts\angular.js" />
+
+(function () {
+    'use strict';
+    //define controller
+    var controllerId = "spAppChrome";
+    //angular.module("FeedbackApp").controller(controllerId, ['$rootScope', spAppChrome]);
+    var feedbackApp = angular.module("FeedbackApp");
+    //create controller
+    function spAppChrome($rootScope)
+    {
+        var vm = this;
+        var spChromeControlData = undefined;
+        //init Controller
+        init();
+        function init() {
+            spChromeControlData = {
+                siteUrl: 'http://www.google.com',
+                siteTitle: 'My DevSite',
+                appTitle: 'Feedback App',
+                settingsLinks: [
+                    {
+                        linkUrl: '/Lists/Announcements',
+                        displayName: '[SHAREPOINT LIST] Announcements'
+                    },
+                    {
+                        linkUrl: '/Lists/Contacts',
+                        displayName: '[SHAREPOINT LIST] Contacts'
+                    }
+
+                ]
+            };
+
+            // create the sharepoint chrome control
+            var nav = new SP.UI.Controls.Navigation("chrome_ctrl_container", spChromeControlData);
+
+            // show chrome control
+            nav.setVisible(true);
+
+            // hide top app chrome (image & app name)
+            nav.setBottomHeaderVisible(false);
+        }
+
+
+
+    }
+
+    feedbackApp.controller(controllerId, spAppChrome);
+
+}());
---
+++ FeedbackApp/App/Services/spContext.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="d:\rahul\poc\sharepointhosted\feedbackapp\feedbackapp\scripts\angular.js" />
+
+(function () {
+    'use strict';
+    var feedbackApp = angular.module('FeedbackApp');
+    var serviceId = 'spContext';
+    function spContext($cookieStore) {
+        var service = this;
+        var spWeb = {
+            appWebUrl: '',
+            url: '',
+            title: '',
+            logoUrl:''
+        };
+        service.hostWeb = spWeb;
+
+        //init the service
+        init();
+        // init... akin to class constructor
+        function init() {
+
+        }
+    }
+}());
