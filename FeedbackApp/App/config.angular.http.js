2b34b84c46c0bd506e3d90bba99cdc592906d447
Rahul <rahul.suryawanshi@outlook.com>
2016-01-27 22:21:04 -0500

    Adding missing files

---
+++ FeedbackApp/App/config.angular.http.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="d:\rahul\poc\sharepointhosted\feedbackapp\feedbackapp\scripts\angular.js" />
+
+(function () {
+    'use strict';
+
+    //define service
+    var serviceId = 'angular.config';
+    function configAngular($http) {
+        //init factory
+
+        init();
+        return {};
+        function init() {
+            //set common http headers
+            $http.defaults.headers.common.Accept = 'application/json;odata=verbose;'
+        }
+    }
+    var feedbackApp = angular.module('FeedbackApp');
+    feedbackApp.factory(serviceId, configAngular);
+}());
---
+++ FeedbackApp/App/config.route.js
@@ -0,0 +1,56 @@
+﻿/// <reference path="d:\rahul\poc\sharepointhosted\feedbackapp\feedbackapp\scripts\angular.js" />
+/// <reference path="d:\rahul\poc\sharepointhosted\feedbackapp\feedbackapp\scripts\angular-route.js" />
+
+
+(function () {
+    'use strict';
+
+    var feedbackApp = angular.module('FeedbackApp');
+
+    //get all the routes
+    feedbackApp.constant('routes', getRoutes());
+
+    //Config routes and their resolver
+
+    feedbackApp.config(['$routeProvider', 'routes', routeConfigurator]);
+
+    function routeConfigurator($routeProvider, routes) {
+        routes.forEach(function (route) {
+            $routeProvider.when(route.url, route.config);
+        });
+        $routeProvider.otherwise({ redirectTo: '/' });//redirect to home of the app
+    }
+
+    //Build the routes
+    function getRoutes() {
+        return [
+            {
+                url: '/',
+                config: {
+                    templateUrl: '../app/dashboard/dashboard.html',
+                    title: 'dashboard',
+                    settings: {
+                        nav: 0,
+                        content: 'dashboard',
+                        quickLaunchEnabled: false
+                    }
+                }
+            },
+            {
+                url: '/Feedbacks',
+                config: {
+                    templateUrl: '../app/FeedbackList/FeedbackList.html',
+                    title: 'Feedbacks',
+                    settings: {
+                        nav: 1,
+                        content: 'Feedback List',
+                        quickLaunchEnabled: true
+                    }
+
+                }
+            }
+        ];
+    }
+
+
+}());
